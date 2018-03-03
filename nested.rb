82b29c8c310436771e801beea6ceff7f782a8c63
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 00:07:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,9 +87,11 @@ def adding_matz
      }
 
 		 programmer_hash[:yukihiro_matsumoto] = {
-			 :known_for => "Ruby"
+			 :known_for => "Ruby", 
 			 :languages => ["LISP", "C"]
 		 }
+
+		 programmer_hash
 
 end
 
